f0c34ebc1cbd6fd876fd2faaad1f024d80acdba6
ryancbrown <16548379+ryancbrown@users.noreply.github.com>
2020-04-01 16:02:07 -0400

    Update user_profile.sql

--- seed/user_profile.sql
+++ seed/user_profile.sql
@@ -1,14 +1,14 @@
 use ecommerce;
 
 create table `user_profile` (
-	`uuid` int(11) not null, 
+    `uuid` int(11) not null, 
     `first_name` varchar(255) not null,
     `last_name` varchar(255) not null,
     `address_line1` varchar(255) not null,
-	`address_line2` varchar(255) null, 
+    `address_line2` varchar(255) null, 
     `city` varchar(255) not null,
     `state` varchar(2) null,
     `postcode` varchar(255) null, 
     `default` boolean not null,
-	primary key (`uuid`)
+     primary key (`uuid`)
 );
