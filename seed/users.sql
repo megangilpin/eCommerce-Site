1691a93b82628f2605ea0e72ed6fc247d4ab6c7c
ryancbrown <16548379+ryancbrown@users.noreply.github.com>
2020-04-01 18:29:37 -0400

    Update users.sql
    Update `uuid` from int to varchar.

--- seed/users.sql
+++ seed/users.sql
@@ -1,7 +1,7 @@
 use ecommerce;
 
 create table `users` (
-    `uuid` int(11) not null, 
+    `uuid` varchar(255) not null, 
     `email` varchar(255) not null,
     `password` varchar(255) not null,
      primary key (`uuid`)
