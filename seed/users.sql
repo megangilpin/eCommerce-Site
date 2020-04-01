0412dbe957ed25ac7c5ed7fb35a38cdcce250513
ryancbrown <16548379+ryancbrown@users.noreply.github.com>
2020-04-01 19:53:41 -0400

    Update users.sql

--- seed/users.sql
+++ seed/users.sql
@@ -4,5 +4,7 @@ create table `users` (
     `uuid` varchar(255) not null, 
     `email` varchar(255) not null,
     `password` varchar(255) not null,
+    `first_name` varchar(255) not null,
+    `last_name` varchar(255) not null,
      primary key (`uuid`)
 );
