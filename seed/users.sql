91a8ca90fb5c2fccccdffb49b4aacac06105b2a8
ryancbrown <16548379+ryancbrown@users.noreply.github.com>
2020-04-01 16:02:28 -0400

    Update users.sql

--- seed/users.sql
+++ seed/users.sql
@@ -1,8 +1,8 @@
 use ecommerce;
 
 create table `users` (
-	`uuid` int(11) not null, 
+    `uuid` int(11) not null, 
     `email` varchar(255) not null,
-	`password` varchar(255) not null,
-	primary key (`uuid`)
+    `password` varchar(255) not null,
+     primary key (`uuid`)
 );
