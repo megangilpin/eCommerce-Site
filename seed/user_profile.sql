d10bb63f7b24f48d2da898750d7fc578cfe44850
rbrown-stripe <16548379+ryancbrown@users.noreply.github.com>
2020-04-01 21:01:46 -0400

    update database seed

--- seed/user_profile.sql
+++ seed/user_profile.sql
@@ -1,14 +1,16 @@
 use ecommerce;
 
 create table `user_profile` (
-	`uuid` int(11) not null, 
+    `uuid` varchar(255) not null, 
+    `type` varchar(255) not null,
     `first_name` varchar(255) not null,
     `last_name` varchar(255) not null,
     `address_line1` varchar(255) not null,
-	`address_line2` varchar(255) null, 
+    `address_line2` varchar(255) null, 
     `city` varchar(255) not null,
     `state` varchar(2) null,
     `postcode` varchar(255) null, 
+    `country` varchar(2) not null, 
     `default` boolean not null,
-	primary key (`uuid`)
+     primary key (`uuid`)
 );
--- seed/users.sql
+++ seed/users.sql
@@ -1,8 +1,10 @@
 use ecommerce;
 
 create table `users` (
-	`uuid` int(11) not null, 
+    `uuid` varchar(255) not null, 
     `email` varchar(255) not null,
-	`password` varchar(255) not null,
-	primary key (`uuid`)
+    `password` varchar(255) not null,
+    `first_name` varchar(255) not null,
+    `last_name` varchar(255) not null,
+     primary key (`uuid`)
 );
