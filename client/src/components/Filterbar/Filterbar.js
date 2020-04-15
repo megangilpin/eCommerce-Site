21220bba6a577a65ed41b833ec681d4eaeed00b1
megangilpin <48898520+megangilpin@users.noreply.github.com>
2020-04-15 09:17:19 -0400

    started filterbar

---
+++ client/src/components/Filterbar/Filterbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Filterbar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Filterbar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react"; 
 import Navbar from "../components/Nav/Navbar";
+import Filterbar from "../components/Filterbar/Filterbar";
 
 
 export default function Home() { 
   return ( 
     <>
       <Navbar />
+      <Filterbar />
     </> 
   );
 };
